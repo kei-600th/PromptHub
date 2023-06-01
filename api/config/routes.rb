4450dd3c6079f56476526ca2bf46509431c44da2
kei-600th <kei19941005@gmail.com>
2023-06-01 17:09:52 +0900

    API側の本番環境への対応

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      # api test action
-      resources :tasks, only:[:index]
+      resources :tasks, only: :index
+      get :health_check, to: 'health_check#index'
     end
   end
 end
