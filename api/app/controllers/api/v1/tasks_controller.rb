49b5af933199bb0d3094a007bf12cf2c62030923
kei-600th <kei19941005@gmail.com>
2023-06-28 18:04:03 +0900

    rubocopの設定と実行

--- api/app/controllers/api/v1/tasks_controller.rb
+++ api/app/controllers/api/v1/tasks_controller.rb
@@ -1,17 +1,14 @@
 class Api::V1::TasksController < ApplicationController
-
   def index
     # この辺りはかなり簡略化しています。
-# 本来であれば seeds.rb を作成した方が良いです...
+    # 本来であれば seeds.rb を作成した方が良いです...
     if Task.count.zero?
       ['1st task', '2nd task', '3rd task'].each do |title|
         Task.create!(title: title)
         puts "Hello CICD!"
       end
     end
-# この辺りも本来であればきちんとシリアライズした方が良いです...
+    # この辺りも本来であればきちんとシリアライズした方が良いです...
     render json: { tasks: Task.all.to_json }
   end
-
-  
 end
