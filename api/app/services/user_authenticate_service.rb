099536e7fb575d31110aaa389e535a5aaa16f070
kei-600th <kei19941005@gmail.com>
2023-07-06 17:31:29 +0900

    rubocop -a

--- api/app/services/user_authenticate_service.rb
+++ api/app/services/user_authenticate_service.rb
@@ -1,5 +1,4 @@
 module UserAuthenticateService
-
   # 認証（トークンの持ち主を判定）
   # 認証済みのユーザーが居ればtrue、存在しない場合は401を返す
   def authenticate_user
@@ -16,28 +15,29 @@ module UserAuthenticateService
 
   private
 
-    # リクエストヘッダートークンを取得する
-    def token_from_request_headers
-      request.headers["Authorization"]&.split&.last
-    end
+  # リクエストヘッダートークンを取得する
+  def token_from_request_headers
+    request.headers["Authorization"]&.split&.last
+  end
 
-    # access_tokenから有効なユーザーを取得する
-    def fetch_user_from_access_token
-      User.from_access_token(token_from_request_headers)
-    rescue UserAuth.not_found_exception_class,
-           JWT::DecodeError, JWT::EncodeError
-      nil
-    end
+  # access_tokenから有効なユーザーを取得する
+  def fetch_user_from_access_token
+    User.from_access_token(token_from_request_headers)
+  rescue UserAuth.not_found_exception_class,
+         JWT::DecodeError, JWT::EncodeError
+    nil
+  end
 
-    # tokenのユーザーを返す
-    def current_user
-      return nil unless token_from_request_headers
-      @_current_user ||= fetch_user_from_access_token
-    end
+  # tokenのユーザーを返す
+  def current_user
+    return nil unless token_from_request_headers
 
-    # 認証エラー
-    def unauthorized_user
-      cookies.delete(UserAuth.session_key)
-      head(:unauthorized)
-    end
+    @_current_user ||= fetch_user_from_access_token
+  end
+
+  # 認証エラー
+  def unauthorized_user
+    cookies.delete(UserAuth.session_key)
+    head(:unauthorized)
+  end
 end
