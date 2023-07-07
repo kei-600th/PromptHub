9f451372b43257841983dc45bcfe7154f38920b9
kei-600th <kei19941005@gmail.com>
2023-07-07 14:26:39 +0900

    認証を行うコントローラーを作成

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tasks, only: :index
-      resources :users, only: [:index]
+      resources :auth_token, only:[:create] do
+        post :refresh, on: :collection
+        delete :destroy, on: :collection
+      end
       get :health_check, to: 'health_check#index'
     end
   end
