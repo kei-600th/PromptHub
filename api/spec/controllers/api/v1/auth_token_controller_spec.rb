98b70f94211bc5ec1c120749bbdf4f06a3cf6b81
kei-600th <kei19941005@gmail.com>
2023-07-10 18:17:24 +0900

    テストケースを分割する

--- api/spec/controllers/api/v1/auth_token_controller_spec.rb
+++ api/spec/controllers/api/v1/auth_token_controller_spec.rb
@@ -26,61 +26,79 @@ RSpec.describe Api::V1::AuthTokenController, type: :controller do
 
   describe "POST #create" do
     context "valid login" do
-      it "returns a 200 response" do
+      before do
         post :create, params: @params, xhr: true
+        @parsed_response = JSON.parse(response.body)
+        @access_token = User.decode_access_token(@parsed_response[@access_token_key])
+        @user.reload
+        @refresh_lifetime_to_i = @refresh_lifetime.from_now.to_i
+      end
+  
+      it "returns a 200 response" do
         expect(response).to have_http_status(200)
-        access_lifetime_to_i = @access_lifetime.from_now.to_i
-        refresh_lifetime_to_i = @refresh_lifetime.from_now.to_i
-
-        # jtiは保存されているか
-        @user.reload
+      end
+  
+      it "saves the jti" do
         expect(@user.refresh_jti).not_to be_nil
-
-        # レスポンスユーザーは正しいか
-        parsed_response = JSON.parse(response.body)
-        expect(parsed_response["user"]["id"]).to eq(@user.id)
-
-        # レスポンス有効期限は想定通りか(1誤差許容)
-        expect(parsed_response["expires"]).to be_within(1).of(access_lifetime_to_i)
-
-        ## access_token
-        access_token = User.decode_access_token(parsed_response[@access_token_key])
-
-        # ユーザーはログイン本人と一致しているか
-        expect(access_token.entity_for_user).to eq(@user)
-
-        # 有効期限はレスポンスと一致しているか
-        token_exp = access_token.payload["exp"]
-        expect(parsed_response["expires"]).to eq(token_exp)
-
-        ## cookie
-        # cookieのオプションを取得する場合は下記を使用
-        # request.cookie_jar.instance_variable_get(:@set_cookies)[<key>]
-        cookie = request.cookie_jar.
-                 instance_variable_get(:@set_cookies)[@session_key]
-
-        # expiresは想定通りか(1秒許容)
-        expect(cookie[:expires]).to be_within(1.seconds).of(Time.at(refresh_lifetime_to_i))
-
-        # secureは一致しているか
-        expect(cookie[:secure]).to eq(Rails.env.production?)
-
-        # http_onlyはtrueか
-        expect(cookie[:http_only]).to be true
-
-        ## refresh_token
-        token_from_cookies = cookies[@session_key]
-        refresh_token = User.decode_refresh_token(token_from_cookies)
-        @user.reload
-
-        # ログイン本人と一致しているか
-        expect(refresh_token.entity_for_user).to eq(@user)
-
-        # jtiは一致しているか
-        expect(@user.refresh_jti).to eq(refresh_token.payload["jti"])
-        # token有効期限とcookie有効期限は一致しているか
-        expect(refresh_token.payload["exp"]).to be_within(1.second).of(refresh_lifetime_to_i)
+      end
+  
+      it "returns the correct user" do
+        expect(@parsed_response["user"]["id"]).to eq(@user.id)
+      end
+  
+      it "sets the correct access token expiration" do
+        access_lifetime_to_i = @access_lifetime.from_now.to_i
+        expect(@parsed_response["expires"]).to be_within(1).of(access_lifetime_to_i)
+      end
+  
+      it "decodes the correct user from access token" do
+        expect(@access_token.entity_for_user).to eq(@user)
+      end
+  
+      it "matches the expiration of the access token with the response" do
+        token_exp = @access_token.payload["exp"]
+        expect(@parsed_response["expires"]).to eq(token_exp)
+      end
+  
+      context "cookies" do
+        before do
+          @cookie = request.cookie_jar.instance_variable_get(:@set_cookies)[@session_key]
+          @refresh_lifetime_to_i = @refresh_lifetime.from_now.to_i
+        end
+  
+        it "sets the correct cookie expiration" do
+          expect(@cookie[:expires]).to be_within(1.seconds).of(Time.at(@refresh_lifetime_to_i))
+        end
+  
+        it "sets the correct cookie secure option" do
+          expect(@cookie[:secure]).to eq(Rails.env.production?)
+        end
+  
+        it "sets the cookie http_only option to true" do
+          expect(@cookie[:http_only]).to be true
+        end
+      end
+  
+      context "refresh token" do
+        before do
+          token_from_cookies = cookies[@session_key]
+          @refresh_token = User.decode_refresh_token(token_from_cookies)
+          @user.reload
+        end
+  
+        it "decodes the correct user from refresh token" do
+          expect(@refresh_token.entity_for_user).to eq(@user)
+        end
+  
+        it "matches the jti with the refresh token" do
+          expect(@user.refresh_jti).to eq(@refresh_token.payload["jti"])
+        end
+  
+        it "matches the expiration of the refresh token with the cookie" do
+          expect(@refresh_token.payload["exp"]).to be_within(1.second).of(@refresh_lifetime_to_i)
+        end
       end
     end
   end
+  
 end
