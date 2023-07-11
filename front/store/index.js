6851873c2ebdbe223787fa87339c7941e77f4ae6
kei-600th <kei19941005@gmail.com>
2023-07-11 16:34:43 +0900

    認証情報をVuexに保存させる

---
+++ front/store/index.js
@@ -0,0 +1,47 @@
+// 変数
+export const state = () => ({
+  user: {
+    current: null
+  },
+  auth: {
+    token: null,
+    expires: 0,
+    payload: {}
+  }
+})
+
+// 算出プロパティ
+export const getters = {}
+// stateの値を変更する場所
+export const mutations = {
+  setCurrentUser (state, payload) {
+    state.user.current = payload
+  },
+  setAuthToken (state, payload) {
+    state.auth.token = payload
+  },
+  setAuthExpires (state, payload) {
+    state.auth.expires = payload
+  },
+  setAuthPayload (state, payload) {
+    state.auth.payload = payload
+  }
+}
+
+// メソッド
+export const actions = {
+  getCurrentUser ({ commit }, user) {
+    commit('setCurrentUser', user)
+  },
+  getAuthToken ({ commit }, token) {
+    commit('setAuthToken', token)
+  },
+  getAuthExpires ({ commit }, expires) {
+    expires = expires || 0
+    commit('setAuthExpires', expires)
+  },
+  getAuthPayload ({ commit }, jwtPayload) {
+    jwtPayload = jwtPayload || {}
+    commit('setAuthPayload', jwtPayload)
+  }
+}
