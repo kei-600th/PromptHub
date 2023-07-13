8ecb5e329278b6d2fe43bfaf8c38edc6992dc431
kei-600th <kei19941005@gmail.com>
2023-07-13 16:48:36 +0900

    会員登録ページの仮作成

--- front/pages/signup.vue
+++ front/pages/signup.vue
@@ -1,10 +1,66 @@
 <template>
-  <div>
-    {{ $route.fullPath }}
-  </div>
+  <user-form-card>
+    <template
+      #user-form-card-content
+    >
+      <v-form
+      ref="form"
+      v-model="isValid"
+      @submit.prevent="signup"
+      >
+        <user-form-name
+          :name.sync="params.user.name"
+        />
+        <user-form-email
+          :email.sync="params.user.email"
+          placeholder
+        />
+        <user-form-password
+          :password.sync="params.user.password"
+          set-validation
+        />
+        <v-btn
+        type="submit"
+        :disabled="!isValid || loading"
+        :loading="loading"
+        block
+        color="appblue"
+        class="white--text"
+      >
+        登録する
+        </v-btn>
+      </v-form>
+      <v-card-text>
+      </v-card-text>
+    </template>
+  </user-form-card>
 </template>
 
 <script>
+
 export default {
+  layout: 'beforeLogin',
+  data () {
+    return {
+      isValid: false,
+      loading: false,
+      params: { user: { name: '', email: '', password: '' } }
+    }
+  },
+  methods: {
+    signup () {
+      this.loading = true
+      setTimeout(() => {
+        this.formReset()
+        this.loading = false
+      }, 1500)
+    },
+    formReset () {
+      this.$refs.form.reset()
+      for (const key in this.params.user) {
+        this.params.user[key] = ''
+      }
+    }
+  }
 }
 </script>
