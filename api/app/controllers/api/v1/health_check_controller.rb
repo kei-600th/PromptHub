501bc1299f24680f1c9a58d2f902717c2cd159ec
kei-600th <kei19941005@gmail.com>
2023-07-15 10:19:07 +0900

    ヘルスチェックの際は:xhr_request?を行わない

--- api/app/controllers/api/v1/health_check_controller.rb
+++ api/app/controllers/api/v1/health_check_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class HealthCheckController < ApplicationController
+
+      # xhr_request?をスキップ
+      skip_before_action :xhr_request?
+
       def index
         head :ok
       end
