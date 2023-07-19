ad4dddbd3534f570a3f56c3e046cc0f5c0197897
kei-600th <kei19941005@gmail.com>
2023-07-19 15:33:03 +0900

    Update entrypoint.sh

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -8,7 +8,7 @@ rm -f /app/tmp/pids/server.pid
 bundle exec rails db:migrate
 
 # Railsサーバーをバックグラウンドで起動
-bundle exec puma -C config/puma.rb &
+bundle exec puma -C config/puma.rb 
 
 # nginxをフォアグラウンドで起動
-exec nginx -g 'daemon off;'
+# exec nginx -g 'daemon off;'
