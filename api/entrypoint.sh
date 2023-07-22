3e8e5478ff393eb326b5114fb2093c88f0b02dad
kei-600th <kei19941005@gmail.com>
2023-07-22 10:23:56 +0900

    起動方法をpumaからrails sに変更

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,3 @@
-# nginxが動作しないためpumaのみで起動する
-
 #!/bin/sh
 set -e
 
@@ -9,8 +7,12 @@ rm -f /app/tmp/pids/server.pid
 # マイグレーションを実行
 bundle exec rails db:migrate
 
+# railsサーバーを起動
+exec bundle exec rails s -p $API_PORT -b '0.0.0.0'
+
+# nginx + pumaの構成
 # Railsサーバーをバックグラウンドで起動
-bundle exec puma -C config/puma.rb
+# bundle exec puma -C config/puma.rb
 
 # nginxをフォアグラウンドで起動
 # exec nginx -g 'daemon off;'
