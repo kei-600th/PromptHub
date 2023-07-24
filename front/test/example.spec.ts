38fbaae598e066d2968b1356282e8a2c04a4c706
kei-600th <kei19941005@gmail.com>
2023-07-24 16:26:47 +0900

    vueファイルをlintで読み込ませる

--- front/test/example.spec.ts
+++ front/test/example.spec.ts
@@ -1,9 +1,9 @@
-import { mount } from "@vue/test-utils";
-import TodoList from "@/components/TodoList.vue";
+import { mount } from '@vue/test-utils'
+import TodoList from '@/components/TodoList.vue'
 
-describe("TodoList.vue", () => {
-  it("renders a todo item", () => {
-    const wrapper = mount(TodoList);
-    expect(wrapper.text()).toContain("todo item");
-  });
-});
+describe('TodoList.vue', () => {
+  it('renders a todo item', () => {
+    const wrapper = mount(TodoList)
+    expect(wrapper.text()).toContain('todo item')
+  })
+})
