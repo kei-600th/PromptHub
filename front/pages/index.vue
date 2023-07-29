e70b4fea12370951f2600e68a2c30a7131dd908d
kei-600th <kei19941005@gmail.com>
2023-07-29 18:23:21 +0900

    サンプル一覧ページの作成

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,9 +1,58 @@
 <template>
   <div>
-    {{ $route.fullPath }}
+    <v-row>
+      <v-col
+      v-for="(sample, index) in samples"
+      :key="index"
+      cols="12"
+      :sm="card.sm"
+      :md="card.md"
+      >
+        <v-card
+        :height="card.height"
+        :elevation="card.elevation"
+        max-width="344"
+        class="mx-auto"
+        >
+          <v-card-title>
+            {{ sample.title }}
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data () {
+    return {
+      samples: [],
+      card: {
+        sm: 6,
+        md: 4,
+        height: 110,
+        elevation: 4
+      },
+    }
+  },
+  mounted() {
+    this.getSample();
+  },
+  methods: {
+    async getSample() {
+      await this.$axios
+      .$get('/api/v1/samples/')
+      .then(
+        (response) => {
+          this.samples = response;
+          console.log(this.samples);
+        }
+      )
+      .catch((error) => {
+        console.log(error)
+      });
+    }
+  },
+};
 </script>
