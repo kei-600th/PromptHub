dfe8c82ba3f6123b1096ef496d8c46bdbed5a001
kei-600th <kei19941005@gmail.com>
2023-07-29 12:00:18 +0900

    バリデーションの追加

--- api/app/models/prompt.rb
+++ api/app/models/prompt.rb
@@ -1,3 +1,6 @@
 class Prompt < ApplicationRecord
   belongs_to :sample
+
+  validates :request_text, presence: true
+  validates :response_text, presence: true
 end
--- api/app/models/sample.rb
+++ api/app/models/sample.rb
@@ -1,3 +1,6 @@
 class Sample < ApplicationRecord
   has_many :prompts, dependent: :destroy
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
