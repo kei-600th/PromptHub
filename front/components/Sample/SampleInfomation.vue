612a42a1a46d7fc68ff4d737a9c3ecf58601154d
kei-600th <kei19941005@gmail.com>
2023-07-30 14:36:33 +0900

    コンポーネントの一部をSampleInfomationとして分離

---
+++ front/components/Sample/SampleInfomation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-card-title class="text-h4">
+      {{ title }}
+    </v-card-title>
+    <v-card-subtitle class="text-h6 my-2">
+      {{ description }}
+    </v-card-subtitle>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- front/pages/sample/_id.vue
+++ front/pages/sample/_id.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="container">
     <v-card>
-      <v-card-title
-      class="text-h4"
-      >
-        {{ sample.title }}
-      </v-card-title>
-      <v-card-subtitle
-      class="text-h6 my-2"
-      >
-        {{ sample.description }}
-      </v-card-subtitle>
       <div
-      v-for="(prompt, index) in sample.prompts"
+        v-if="sampleEditting === false"
+      >
+        <SampleInfomation 
+          v-if="params.sample.title && params.sample.description"
+          :title="params.sample.title"
+          :description="params.sample.description"
+        />
+      </div>
+      <div
+        v-if="sampleEditting === true"
+      >
+        <SampleForm 
+          :title="params.sample.title"
+          :description="params.sample.description"
+          @updateTitle="params.sample.title = $event"
+          @updateDescription="params.sample.description = $event"
+        />
+      </div>
+      <div
+      v-for="(prompt, index) in params.sample.prompts"
       :key="index"
       >
         <ChatLog 
@@ -20,36 +29,89 @@
           :responseText="prompt.response_text"
         />
       </div>
+      <v-row class="justify-end">
+        <div
+          v-if="sampleEditting === false"
+        >
+          <v-btn
+            color="appblue"
+            class="white--text ma-5"
+            @click="editSample()"
+          >
+            サンプルを編集
+          </v-btn>
+          <v-btn
+            color="appblue"
+            class="white--text ma-5"
+          >
+            サンプルを削除
+          </v-btn>
+        </div>
+        <div
+          v-if="sampleEditting === true"
+        >
+          <v-btn
+            :disabled="anyIsEmptyOrWhitespace(params.sample.title, params.sample.description) || loading"
+            :loading="loading"
+            color="appblue"
+            class="white--text ma-5"
+          >
+            編集を保存
+          </v-btn>
+          <v-btn
+            color="appblue"
+            class="white--text ma-5"
+            @click="cancelEditSample()"
+          >
+            編集を取消
+          </v-btn>
+        </div>
+      </v-row>
     </v-card>
   </div>
 </template>
 
 <script>
 import ChatLog from '@/components/Sample/ChatLog.vue';
+import SampleInfomation from '@/components/Sample/SampleInfomation.vue';
 export default {
   components: {
-    ChatLog
+    ChatLog,
+    SampleInfomation
   },
   data () {
     return {
-      sample: {},
+      loading: false,
+      params: {
+        sample: {},
+      },
+      sampleEditting: false,
     }
   },
   async mounted() {
     await this.getSample();
   },
   methods: {
+    anyIsEmptyOrWhitespace(...texts) {
+      return texts.some(text => text.trim() === '');
+    },
     async getSample() {
       const id = this.$route.params.id;
       await this.$axios
       .$get(`/api/v1/samples/${id}`)
       .then((response) => {
-        this.sample = response;
-        console.log(this.sample);
+        this.params.sample = response;
       })
       .catch((error) => {
         console.log(error);
       });
+    },
+    editSample() {
+      this.sampleEditting = true
+    },
+    async cancelEditSample() {
+      await this.getSample()
+      this.sampleEditting = false
     }
   }
 }
