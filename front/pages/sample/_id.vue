e0f780ed009cd32793134a7c936f8de4aef7e2cf
kei-600th <kei19941005@gmail.com>
2023-07-30 11:58:39 +0900

    front側にサンプル詳細ページの追加

--- front/pages/sample/_id.vue
+++ front/pages/sample/_id.vue
@@ -1,9 +1,56 @@
 <template>
-  <div>
-    {{ $route.fullPath }}
+  <div class="container">
+    <v-card>
+      <v-card-title
+      class="text-h4"
+      >
+        {{ sample.title }}
+      </v-card-title>
+      <v-card-subtitle
+      class="text-h6 my-2"
+      >
+        {{ sample.description }}
+      </v-card-subtitle>
+      <div
+      v-for="(prompt, index) in sample.prompts"
+      :key="index"
+      >
+        <ChatLog 
+          :requestText="prompt.request_text" 
+          :responseText="prompt.response_text"
+        />
+      </div>
+    </v-card>
   </div>
 </template>
 
 <script>
-export default {};
+import ChatLog from '@/components/Sample/ChatLog.vue';
+export default {
+  components: {
+    ChatLog
+  },
+  data () {
+    return {
+      sample: {},
+    }
+  },
+  async mounted() {
+    await this.getSample();
+  },
+  methods: {
+    async getSample() {
+      const id = this.$route.params.id;
+      await this.$axios
+      .$get(`/api/v1/samples/${id}`)
+      .then((response) => {
+        this.sample = response;
+        console.log(this.sample);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  }
+}
 </script>
