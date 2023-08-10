13018779a85af825c48a64fc9bd56d9df4919def
kei-600th <kei19941005@gmail.com>
2023-08-10 14:47:21 +0900

    sampleコントローラーから余計なアクションを削除

--- api/app/controllers/api/v1/admin/samples_controller.rb
+++ api/app/controllers/api/v1/admin/samples_controller.rb
@@ -9,14 +9,6 @@ class Api::V1::Admin::SamplesController < ApplicationController
     render json: sample
   end
 
-  def new
-    prompt = Prompt.new(prompt_params)
-    prompt.response_text = get_response_text(prompt.request_text)
-    render json: prompt
-  rescue StandardError => e
-    render json: { error: e.message }, status: :unprocessable_entity
-  end
-
   def create
     sample = Sample.new(sample_params)
     if sample.save
@@ -44,13 +36,6 @@ class Api::V1::Admin::SamplesController < ApplicationController
     end
   end
 
-  def get_response_text(request)
-    response = openai_chat(request)
-    response.dig("choices", 0, "message", "content")
-  rescue StandardError => e
-    raise e.message
-  end
-
   private
 
   def handle_sample_creation_success(sample)
@@ -62,25 +47,6 @@ class Api::V1::Admin::SamplesController < ApplicationController
 
   def handle_sample_creation_failure
     render json: { error: "サンプルの作成に失敗しました。" }, status: :unprocessable_entity
-  end
-
-  def openai_chat(request)
-    client = initialize_openai_client
-    response = client.chat(
-      parameters: {
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: request }],
-        temperature: 0.7
-      }
-    )
-    raise StandardError, "OpenAIからの応答でエラーが発生しました: #{response['error']['message']}" if response["error"]
-
-    response
-  end
-
-  def initialize_openai_client
-    require "openai"
-    OpenAI::Client.new(access_token: ENV.fetch('OPENAI_API_KEY', nil))
   end
 
   def prompt_params
