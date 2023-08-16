7971c2c6984f876538aca9e00e896550df6053bb
kei-600th <kei19941005@gmail.com>
2023-08-16 13:03:09 +0900

    samplesテーブルにFKの追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_14_071851) do
+ActiveRecord::Schema.define(version: 2023_08_16_035152) do
+
+  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "prompts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "sample_id", null: false
@@ -27,6 +33,8 @@ ActiveRecord::Schema.define(version: 2023_08_14_071851) do
     t.text "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "fk_rails_d2c396ee23"
   end
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -47,4 +55,5 @@ ActiveRecord::Schema.define(version: 2023_08_14_071851) do
   end
 
   add_foreign_key "prompts", "samples"
+  add_foreign_key "samples", "categories"
 end
