28e6efd25c46e199ce4aef2a3b790b7c1c5b4263
kei-600th <kei19941005@gmail.com>
2023-08-19 19:25:39 +0900

    categoryモデルテストの作成

--- api/app/models/category.rb
+++ api/app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
 
   has_many :samples
+
+  validates :name, presence: true
 end
--- api/spec/models/category_spec.rb
+++ api/spec/models/category_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { FactoryBot.build(:category) }
+
+  describe 'バリデーション' do
+    context 'nameとimageがあれば' do
+      it '有効であること' do
+        expect(category).to be_valid
+      end
+    end
+
+    context 'nameが空の場合' do
+      before { category.name = '' }
+
+      it '無効であること' do
+        expect(category).not_to be_valid
+      end
+    end
+
+    context 'imageが空の場合' do
+      before { category.image = '' }
+
+      it '有効であること' do
+        expect(category).to be_valid
+      end
+    end
+  end
 end
