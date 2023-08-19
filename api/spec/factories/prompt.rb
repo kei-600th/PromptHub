29fd5f4dfb06b12f16d372c3aace34bd9419625b
kei-600th <kei19941005@gmail.com>
2023-08-19 17:47:19 +0900

    promptのfactorybotにカラムを追加

--- api/spec/factories/prompt.rb
+++ api/spec/factories/prompt.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :prompt do
     sequence(:request_text) { |n| "質問です。#{n}" }
     sequence(:response_text) { |n| "回答です。#{n}" }
+    sequence(:gpt_model) { "gpt-3.5-turbo" }
     sample
   end
 end
