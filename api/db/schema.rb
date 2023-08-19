1dd1c3ee57aff6b25e567af23ce0df7dc75e17d3
kei-600th <kei19941005@gmail.com>
2023-08-19 14:14:10 +0900

    画像url保存用カラムの作成

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_16_035152) do
+ActiveRecord::Schema.define(version: 2023_08_19_033625) do
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "prompts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
