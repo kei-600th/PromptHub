2bf31eccb2713158f1d97a223727a423ce47df5c
kei-600th <kei19941005@gmail.com>
2023-08-20 15:45:39 +0900

    front側でカテゴリの画像を参照する

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -21,7 +21,7 @@
           class="v-btn text-capitalize mx-auto"
         >
           <v-img
-            :src="sample.category.image"
+            :src="images[(sample.category_id - 1)]"
             height="150"
             class="white--text align-end"
           >
@@ -57,6 +57,16 @@ export default {
         height: 150,
         elevation: 4,
       },
+      images: [
+        require('@/assets/images/sample_images/pc.jpeg'),
+        require('@/assets/images/sample_images/talking.jpeg'),
+        require('@/assets/images/sample_images/buisiness.jpeg'),
+        require('@/assets/images/sample_images/heart.jpeg'),
+        require('@/assets/images/sample_images/academy.jpeg'),
+        require('@/assets/images/sample_images/house.jpeg'),
+        require('@/assets/images/sample_images/books.jpeg'),
+        require('@/assets/images/sample_images/designing.jpeg')
+      ],
     };
   },
   watch: {
