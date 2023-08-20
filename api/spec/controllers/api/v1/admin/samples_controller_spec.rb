371998349f33fa8b99cef949a4d0f6a022c9940e
kei-600th <kei19941005@gmail.com>
2023-08-20 11:10:28 +0900

    factorybotの修正

--- api/spec/controllers/api/v1/admin/samples_controller_spec.rb
+++ api/spec/controllers/api/v1/admin/samples_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Admin::SamplesController do
+  let(:user_params) { FactoryBot.attributes_for(:user) }
+  let(:admin_user) { FactoryBot.create(:user, admin: true) }
+
   describe 'POST #create' do
     let(:category) { FactoryBot.create(:category) }
     let(:sample_params) { FactoryBot.attributes_for(:sample, category_id: category.id) }
     let(:prompt_params) { FactoryBot.attributes_for(:prompt) }
-    let(:admin_user) { FactoryBot.create(:user, admin: true) }
 
     it 'creates a new sample' do
       expect do
@@ -14,9 +16,8 @@ RSpec.describe Api::V1::Admin::SamplesController do
     end
 
     it 'does not create a new sample with non-admin user' do
-      user = FactoryBot.attributes_for(:user)
       expect do
-        post :create, params: { sample: sample_params, prompt: prompt_params, user: user }, xhr: true
+        post :create, params: { sample: sample_params, prompt: prompt_params, user: user_params }, xhr: true
       end.not_to change(Sample, :count)
     end
 
@@ -29,13 +30,10 @@ RSpec.describe Api::V1::Admin::SamplesController do
 
   describe 'PATCH #update' do
     let!(:sample) { FactoryBot.create(:sample, title: "Old Title") }
-    let(:user_params) { FactoryBot.attributes_for(:user) }
-    let(:admin_user) { FactoryBot.create(:user, admin: true) }
-    let(:admin_user_params) { admin_user.attributes }
 
     it 'updates the sample with admin user' do
       new_title = "New Title"
-      patch :update, params: { id: sample.id, sample: { title: new_title }, user: admin_user_params }, xhr: true
+      patch :update, params: { id: sample.id, sample: { title: new_title }, user: admin_user.attributes }, xhr: true
       expect(sample.reload.title).to eq(new_title)
     end
 
@@ -54,13 +52,10 @@ RSpec.describe Api::V1::Admin::SamplesController do
 
   describe 'DELETE #destroy' do
     let!(:sample) { FactoryBot.create(:sample) }
-    let(:user_params) { FactoryBot.attributes_for(:user) }
-    let(:admin_user) { FactoryBot.create(:user, admin: true) }
-    let(:admin_user_params) { admin_user.attributes }
 
     it 'deletes the sample' do
       expect do
-        delete :destroy, params: { id: sample.id, user: admin_user_params }, xhr: true
+        delete :destroy, params: { id: sample.id, user: admin_user.attributes }, xhr: true
       end.to change(Sample, :count).by(-1)
     end
 
