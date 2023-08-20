67a1104710572eedc9598d6d97b48cb67091db21
kei-600th <kei19941005@gmail.com>
2023-08-20 10:05:23 +0900

    rubocop -aの実行

--- api/app/controllers/api/v1/samples_controller.rb
+++ api/app/controllers/api/v1/samples_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::SamplesController < ApplicationController
   def index
-    if params[:category_id]
-      samples = Sample.includes(:category).where(category_id: params[:category_id])
-    else
-      samples = Sample.includes(:category).all
-    end
+    samples = if params[:category_id]
+                Sample.includes(:category).where(category_id: params[:category_id])
+              else
+                Sample.includes(:category).all
+              end
     render json: samples
   end
 
