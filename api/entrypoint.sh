493e28eac92aa4daca8206f40354355e44e65dd0
kei-600th <kei19941005@gmail.com>
2023-08-25 11:35:08 +0900

    nginx + pumaで構成

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -8,11 +8,11 @@ rm -f /app/tmp/pids/server.pid
 bundle exec rails db:migrate
 
 # railsサーバーを起動
-exec bundle exec rails s -p $API_PORT -b '0.0.0.0'
+# exec bundle exec rails s -p $API_PORT -b '0.0.0.0'
 
 # nginx + pumaの構成
 # Railsサーバーをバックグラウンドで起動
-# bundle exec puma -C config/puma.rb
+bundle exec puma -C config/puma.rb &
 
 # nginxをフォアグラウンドで起動
-# exec nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
