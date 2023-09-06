cccb5968ce928c4e794580b99927031e9db9364f
kei-600th <kei19941005@gmail.com>
2023-09-06 17:04:43 +0900

    プロダクト紹介ページの作成

--- front/pages/about.vue
+++ front/pages/about.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="eye-catching-container">
-    <div class="title-container">
-      <h1>ChatGPTの使い方を共有しよう</h1>
+  <div>
+    <div class="eye-catching-container">
+      <div>
+        <h1>ChatGPTの使い方を広げよう</h1>
+      </div>
+      <div>
+        <v-img :src="require('@/assets/images/introduce/prompthub.jpeg')" alt="Prompthub" class="eye-catching-img"></v-img>
+      </div>
     </div>
-    <div class="img-container">
-      <v-img :src="require('@/assets/images/sample_images/prompthub.jpeg')" alt="Prompthub" class="custom-img"></v-img>
+    <div class="features-container">
+      <h1>Prompthubの使い方</h1>
+      <div class="features-row">
+        <div class="how-to-use">
+          <h2>ChatGPTの参考例を見つける</h2>
+          <p>自分に合った参考例を見つけましょう</p>
+        </div>
+        <div class="card-img-container">
+          <v-img :src="require('@/assets/images/introduce/lunch-break-e1.png')" class="card-img"></v-img>
+        </div>
+        <div class="how-to-use">
+          <h2>実際に使ってみる</h2>
+          <p>サンプルをカスタマイズして実際に使ってみましょう</p>
+        </div>
+        <div class="card-img-container">
+          <v-img :src="require('@/assets/images/introduce/user-interface-8.png')" class="card-img"></v-img>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -14,7 +35,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .eye-catching-container {
   position: relative;
   background-color: #D6D2E0;
@@ -23,16 +44,37 @@ export default {
   justify-content: space-around;
 }
 
-.title-container {
-  margin-right: 20px;
-}
-
-.img-container {
-}
-
-.custom-img {
+.eye-catching-img {
   max-width: 400px;
   max-height: 400px;
-  border-radius: 15px;
+  border-radius: 45px;
 }
+
+.features-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-top: 50px;
+}
+
+.features-row {
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  grid-gap: 20px; /* 各要素の間隔 */
+  margin-top: 50px;
+}
+
+.how-to-use {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.card-img-container {
+}
+
+.card-img {
+}
+
 </style>
