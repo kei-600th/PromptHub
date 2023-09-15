1c5f741515107833744509b0d0d20264926423ef
kei-600th <kei19941005@gmail.com>
2023-09-15 12:10:30 +0900

    エラーページの作成

---
+++ front/layouts/error.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="error-page">
+    <div v-if="error.statusCode === 404" class="error-content">
+      <h1 class="error-title" >404 Not Found</h1>
+      <p class="error-text">お探しのページは見つかりませんでした</p>
+    </div>
+    <div v-else class="error-content">
+      <h1 class="error-title">エラーが発生しました</h1>
+      <p class="error-text">{{ error.message }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+.error-content {
+  margin-top: 50px;
+  text-align: center;
+}
+.error-text {
+  margin-top:30px;
+}
+</style>
