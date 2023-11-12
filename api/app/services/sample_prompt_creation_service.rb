2e9a05e56421ef15af3286d0ecc652f903634f57
kei-600th <kei19941005@gmail.com>
2023-11-12 14:54:07 +0900

    不要な変数の削除

--- api/app/services/sample_prompt_creation_service.rb
+++ api/app/services/sample_prompt_creation_service.rb
@@ -39,7 +39,7 @@ class SamplePromptCreationService
   end
 
   def decode_image(data)
-    content_type, encoding, string = data.match(/\Adata:(.*?);(.*?),(.*)\z/).captures
+    string = data.match(/\Adata:(.*?);(.*?),(.*)\z/).captures
     StringIO.new(Base64.decode64(string))
   end
 end
